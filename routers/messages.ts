1f511e0c8cd9e5ee11beb203a8c8f68ac7bfa019
Atabek <aakmataliev04@gmail.com>
2024-12-02 07:25:43 +0600

    Wrote logic for post request for messages.ts to add messages in txt format with Name made from dateTime

--- routers/messages.ts
+++ routers/messages.ts
@@ -1,12 +1,19 @@
-import express from "express";
+import express from 'express';
+import {promises as fs} from 'fs';
 
 const messagesRouter = express.Router();
 
 messagesRouter.get("/", async (req: express.Request, res: express.Response) => {
-  res.send('All messages');
+  res.send('Messages');
 })
 messagesRouter.post("/", async (req: express.Request, res: express.Response) => {
-  res.send('Created message');
+
+  const dateTime = new Date().toISOString();
+  const messageText = req.body;
+  const message = {...messageText, dateTime: dateTime};
+
+  await fs.writeFile(`./messages/${dateTime}.txt`, JSON.stringify(messageText));
+  res.send(message);
 })
 
 export default messagesRouter;
